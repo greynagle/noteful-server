2f8100e110d9ad7a00b4a927bbeefc1eb2452c28
Grey Nagle <greynagle@gmail.com>
2020-06-14 23:15:36 -0400

    cors update

--- src/app.js
+++ src/app.js
@@ -1,28 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const validateBearerToken = require('./validate-bearer-token')
-const errorHandler = require('./error-handler')
-const notefulRouter = require('./noteful/noteful-router')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const validateBearerToken = require("./validate-bearer-token");
+const errorHandler = require("./error-handler");
+const notefulRouter = require("./noteful/noteful-router");
 
-const app = express()
+const app = express();
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test'
-}))
-app.use(cors())
-app.use(helmet())
+app.use(
+    morgan(NODE_ENV === "production" ? "tiny" : "common", {
+        skip: () => NODE_ENV === "test",
+    })
+);
+app.use(cors());
+app.use(helmet());
 // app.use(validateBearerToken)
 
-app.use(notefulRouter)
+let allowCrossDomain = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+};
+app.use(allowCrossDomain);
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+app.use(notefulRouter);
 
-app.use(errorHandler)
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
 
-module.exports = app
+app.use(errorHandler);
+
+module.exports = app;
